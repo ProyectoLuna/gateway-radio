9869ea6346a68b5f9e193fdd5ee9473de2f3c212
Miguel Garcia <mgm.mgm.85@gmail.com>
2017-07-18 00:23:54 +0200

    gateway-radio
    rf24mesh master hello word

---
+++ src/main.cpp
@@ -0,0 +1,55 @@
+/** @mainpage proyecto-luna - None
+ *
+ * @author migarma <migarma2@gmail.com>
+ * @version 0.0.1
+**/
+
+#include "RF24Mesh/RF24Mesh.h"
+#include <RF24/RF24.h>
+#include <RF24Network/RF24Network.h>
+
+RF24 radio(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ);
+RF24Network network(radio);
+RF24Mesh mesh(radio,network);
+
+int main(int argc, char** argv) {
+
+ // Set the nodeID to 0 for the master node
+ mesh.setNodeID(0);
+ // Connect to the mesh
+ printf("start\n");
+ mesh.begin();
+ radio.printDetails();
+
+while(1)
+{
+
+ // Call network.update as usual to keep the network updated
+ mesh.update();
+
+ // In addition, keep the 'DHCP service' running on the master node so addresses will
+ // be assigned to the sensor nodes
+ mesh.DHCP();
+
+
+ // Check for incoming data from the sensors
+ while(network.available()){
+//    printf("rcv\n");
+   RF24NetworkHeader header;
+   network.peek(header);
+
+   uint32_t dat=0;
+   switch(header.type){
+     // Display the incoming millis() values from the sensor nodes
+     case 'M': network.read(header,&dat,sizeof(dat));
+               printf("Rcv %u from 0%o\n",dat,header.from_node);
+                break;
+     default:  network.read(header,0,0);
+               printf("Rcv bad type %d from 0%o\n",header.type,header.from_node);
+               break;
+   }
+ }
+delay(2);
+ }
+return 0;
+}
